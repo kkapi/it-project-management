74c1802fb4a62c76c7a06296dd8f085fee2fd29e
kkapi <hZG2fik_j8@mail.ru>
2022-10-25 18:51:59 +0300

    add controllers

---
+++ server/controllers/foodController.js
@@ -0,0 +1,15 @@
+class FoodController {
+    async create(req, res) {
+
+    }
+
+    async getAll(req, res) {
+        
+    }
+
+    async getOne(req, res) {
+        
+    }
+}
+
+module.exports = new FoodController()
