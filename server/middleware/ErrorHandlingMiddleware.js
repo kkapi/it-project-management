777347de740615bb92fc1e8c1f91d182f59732db
kkapi <hZG2fik_j8@mail.ru>
2022-11-01 10:46:01 +0300

    modified errorhandler

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -1,8 +1,9 @@
-const ApiError = require('../error/ApiError')
+const ApiError = require('../exeptions/apiError')
 
 module.exports = function (err, req, res, next) {
+    console.log(err)
     if(err instanceof ApiError) {
-        return res.status(err.status).json({message: err.message})
+        return res.status(err.status).json({message: err.message, errors: err.errors})
     }
-    return res.status(500).json({message: "Непредвиденная ошибка!"})
+    return res.status(500).json({message: 'Непредвиденная ошибка'})
 }
