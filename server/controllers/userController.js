d5292e27aced80fc4316db534e7df5e354871ea3
kkapi <hZG2fik_j8@mail.ru>
2022-11-01 10:47:33 +0300

    modified userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,52 +1,85 @@
-const ApiError = require('../error/ApiError')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const {User, Basket} = require('../models/models')
+const userService = require('../service/userService')
+const {validationResult} = require('express-validator');
+const ApiError = require('../exeptions/apiError');
 
-const generateJwt = (id, email, role) => {
-    return jwt.sign(
-        {id, email, role},
-        process.env.SECRET_KEY,
-        {expiresIn: '24h'}
-    )
-}
 
 class UserController {
     async registration(req, res, next) {
-        const {email, password, role} = req.body
-        if(!email || !password) {
-            return next(ApiError.badRequest('Некореектный email или пароль'))
+        try {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
+            }
+            const {email, password} = req.body;
+            const userData = await userService.registration(email, password);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 1000, httpOnly: true})
+            return res.json(userData)
+        } catch (e) {
+            next(e);
         }
-        const candidate = await User.findOne({where: {email}})
-        if (candidate) {
-            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
-        }
-        const hashPassword = await bcrypt.hash(password, 5)
-        const user = await User.create({email, role, password: hashPassword})
-        const basket = await Basket.create({userId: user.id})
-        const token = generateJwt(user.id, user.email, user.role)
-        return res.json({token})
-
     }
 
     async login(req, res, next) {
-        const  {email, password} = req.body
-        const user = await User.findOne({where: {email}})
-        if (!user) {
-            return next(ApiError.internal('Пользователь не найден'))
+        try {
+            const {email, password} = req.body;
+            const userData = await userService.login(email, password);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 1000, httpOnly: true})
+            return res.json(userData)
+        } catch (e) {
+            next(e)
         }
-        let comparePassword = bcrypt.compareSync(password, user.password)
-        if (!comparePassword) {
-            return next(ApiError.internal('Указан неверный пароль'))
-        }
-        const token = generateJwt(user.id, user.email, user.role)
-        return res.json({token})
     }
 
-    async check(req, res, next) {
-        const token = generateJwt(req.user.id, req.user.email, req.user.user.role)
-        return res.json({token})
+    async logout(req, res, next) {
+        try {
+            const {refreshToken} = req.cookies;
+            const token = await userService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json(token)
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async activate(req, res, next) {
+        try {
+            const activationLink = req.params.link;
+            await userService.activate(activationLink);
+            return res.redirect(process.env.CLIENT_URL)
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async refresh(req, res, next) {
+        try {
+            const {refreshToken} =  req.cookies;
+            const userData = await userService.refresh(refreshToken);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 1000, httpOnly: true})
+            return res.json(userData)
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async getUsers(req, res, next) {
+        try {
+            const users = await userService.getAllUsers();
+            return res.json(users)
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async passRecover(req, res, next) {
+        try {
+            const {email} = req.body;
+            await userService.recover(email);
+            return res.json(email)
+        } catch(e) {
+            next(e)
+        }
     }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
