21cd65f6bb889b0ca6434928f63dbbc3d8128527
kkapi <hZG2fik_j8@mail.ru>
2022-11-01 10:43:55 +0300

    modified routers

--- server/routes/foodRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const foodController = require('../controllers/foodController')
-
-router.post('/', foodController.create)
-router.get('/', foodController.getAll)
-router.get('/:id', foodController.getOne)
-
-module.exports = router
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,19 @@
-const Router = require('express')
-const router = new Router()
-const foodRouter = require('./foodRouter')
-const typeRouter = require('./typeRouter')
-const userRouter = require('./userRouter')
+const Router = require('express').Router;
+const userController = require('../controllers/userController')
+const router = new Router();
+const {body} = require('express-validator');
+const authMiddleware = require('../middlewares/authMiddlware')
 
-router.use('/user', userRouter)
-router.use('/type', typeRouter)
-router.use('/food', foodRouter)
+router.post('/registration', 
+    body('email').isEmail(),
+    body('password').isLength({min: 3, max: 32}), 
+    userController.registration
+);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', authMiddleware, userController.getUsers);
+router.post('/recovery', userController.passRecover);
 
 module.exports = router
