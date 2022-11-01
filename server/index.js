56ee1c183b6ed681e1ad4eedb09626d4909b34c7
kkapi <hZG2fik_j8@mail.ru>
2022-11-01 10:42:22 +0300

    modified index.js

--- server/index.js
+++ server/index.js
@@ -1,31 +1,30 @@
 require('dotenv').config()
-const express =  require('express')
+const  express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const sequelize = require('./db')
 const models = require('./models/models')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const router = require('./routes/index')
-const errorHandler = require('./middleware/ErrorHandlingMiddleware')
-const path = require('path')
+const router = require('./router/index')
+const errorMiddleware = require('./middlewares/errorMiddleware')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpload({}))
-app.use('/api', router)
 
-//Обработка ошибок, последний middleware
-app.use(errorHandler)
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use('/api', router);
+app.use(errorMiddleware);
 
 const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-
-        app.listen(PORT,  () => console.log(`Server starter on port ${PORT}`))
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
     }
