0665f371965e5c7afa5839c4ff19332854429684
kkapi <hZG2fik_j8@mail.ru>
2022-11-24 21:18:39 +0300

    updated sever/index.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
 require('dotenv').config()
+
 const  express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
 const sequelize = require('./db')
 const models = require('./models/models')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
-const errorMiddleware = require('./middleware/authMiddleware')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL
-}));
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router);
-app.use(errorMiddleware);
+app.use(errorHandler);
 
 const start = async () => {
     try {
