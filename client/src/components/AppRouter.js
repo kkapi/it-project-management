f9e83d41562df1a12ca48a86cb20ac0fe1d73290
kkapi <hZG2fik_j8@mail.ru>
2022-11-26 05:39:32 +0300

    move routes

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import {Routes, Route, Navigate} from 'react-router-dom'
+import { authRoutes } from '../../routes'
+import { LOGIN_ROUTE } from '../utils/consts'
 
 const AppRouter = () => {
+  const isAuth = false
   return (
-    <div>AppRouter</div>
+    <Routes>
+      {authRoutes.map(({path, Component}) => 
+        <Route key={path} path={path} element={<Component/>} exact/>
+      )}
+    </Routes>
   )
 }
 
