7bd2296f319d172d06f2b4b079943e2b77fa3596
kkapi <hZG2fik_j8@mail.ru>
2022-11-26 06:04:33 +0300

    add FoodStore

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Routes, Route, Navigate} from 'react-router-dom'
+import { Context } from '..'
 import { authRoutes, publicRoutes } from '../routes'
 import { LOGIN_ROUTE } from '../utils/consts'
 
 const AppRouter = () => {
-  const isAuth = true
+  const {user} = useContext(Context)
+  console.log(user)
   return (
     <Routes>
-        {isAuth && authRoutes.map(({path, Component}) => 
+        {user.isAuth && authRoutes.map(({path, Component}) => 
             <Route key={path} path={path} element={<Component/>} exact/>
         )}
         {publicRoutes.map(({path, Component}) =>
