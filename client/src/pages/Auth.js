2f93b19a986e6a894f1ab74e5a6900546aa6040a
kkapi <hZG2fik_j8@mail.ru>
2022-11-26 16:18:56 +0300

    done Auth page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Button, Card, Container, Form } from 'react-bootstrap'
+import { NavLink, useLocation } from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
 
 const Auth = () => {
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE
+
   return (
-    <div>Auth</div>
+    <Container 
+      className="d-flex justify-content-center align-items-center"
+      style={{height: window.innerHeight - 54}}
+    >
+      <Card style={{width: 600}} className="p-5">
+        <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+        <Form className="d-flex flex-column">
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш email..."
+          />
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш пароль..."
+          />
+          <Form className="d-flex justify-content-between mt-3">
+            {isLogin ? 
+              <div>
+                  Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйтесь!</NavLink>
+              </div>
+              :
+              <div>
+                  Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+              </div>
+              }
+            <Button
+              className="mt-3"
+              variant="outline-success"            
+            >
+              {isLogin ? 'Войти' : 'Зарегистрироваться'}
+            </Button>
+          </Form>          
+        </Form>
+      </Card>
+    </Container>
   )
 }
 
