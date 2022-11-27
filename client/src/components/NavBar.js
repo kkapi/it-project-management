bdcb9a0a89121daf7a0cfe58caa29fcbf4f352f2
kkapi <hZG2fik_j8@mail.ru>
2022-11-27 03:34:22 +0300

    changed NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
-import { MODERATOR_ROUTE, SHOP_ROUTE } from '../utils/consts';
+import { LOGIN_ROUTE, MODERATOR_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import {Button} from 'react-bootstrap'
 import {observer} from 'mobx-react-lite'
 import { useNavigate } from "react-router-dom"
@@ -17,24 +17,60 @@ const NavBar = observer(() => {
     const logOut = () => {
         user.setUser({})
         user.setIsAuth(false)
+        user.setRole({})
         localStorage.removeItem("token");
     }
 
-  return (
-    <Navbar bg="dark" variant="dark">
-    <Container>
-      <NavLink style={{color: 'white'}} to={SHOP_ROUTE} className="text-decoration-none">EtuFood</NavLink>
-      {user.isAuth ?
-            <Nav className="ml-auto">
-            <Button variant="outline-light" onClick={() => navigate(MODERATOR_ROUTE)}>Панель модератора</Button>
-            <Button variant="outline-light" className="ms-2" onClick={() => logOut()}>Выйти</Button>            
-          </Nav>
-          :
-          <div></div>
-          }
-    </Container>
-  </Navbar>
-  )
+    function drawNavbar(user) {
+      if (user.isAuth) {
+        if (user.role === 'MODERATOR') {
+          return (
+            <Navbar bg="dark" variant="dark">
+              <Container>
+                <NavLink style={{color: 'white'}} to={SHOP_ROUTE} className="text-decoration-none">EtuFood</NavLink>
+                    <Nav className="ml-auto">
+                      <Button variant="outline-light" onClick={() => navigate(MODERATOR_ROUTE)}>Панель модератора</Button>
+                      <Button variant="outline-light" className="ms-2" onClick={() => logOut()}>Выйти</Button>            
+                    </Nav>
+              </Container>
+            </Navbar>
+          )
+        } else if (user.role === 'ADMIN') {
+          return (
+            <Navbar bg="dark" variant="dark">
+              <Container>
+                <NavLink style={{color: 'white'}} to={SHOP_ROUTE} className="text-decoration-none">EtuFood</NavLink>
+                    <Nav className="ml-auto">
+                      <Button variant="outline-light" onClick={() => navigate(MODERATOR_ROUTE)}>Панель модератора</Button>
+                      <Button variant="outline-light" className="ms-2" onClick={() => logOut()}>Выйти</Button>            
+                    </Nav>
+              </Container>
+            </Navbar>
+          )
+        } else {
+          return (
+            <Navbar bg="dark" variant="dark">
+              <Container>
+                <NavLink style={{color: 'white'}} to={SHOP_ROUTE} className="text-decoration-none">EtuFood</NavLink>
+                    <Nav className="ml-auto">                      
+                      <Button variant="outline-light" className="ms-2" onClick={() => logOut()}>Выйти</Button>            
+                    </Nav>
+              </Container>
+            </Navbar>
+          )
+        }        
+      } else {
+        return (
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <NavLink style={{color: 'white'}} to={SHOP_ROUTE} className="text-decoration-none">EtuFood</NavLink>                  
+            </Container>
+          </Navbar>
+        )
+      }
+    }
+
+    return drawNavbar(user)
 })
 
 export default NavBar
