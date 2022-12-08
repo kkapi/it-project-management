990a899d8de58cd888afebafd3b9664772dd8c03
kkapi <hZG2fik_j8@mail.ru>
2022-12-09 00:55:13 +0300

    add food from food page

--- client/src/pages/FoodPage.js
+++ client/src/pages/FoodPage.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { Button, Card, Col, Container, Form, Image, Row } from 'react-bootstrap'
 import {useParams} from 'react-router-dom'
 import { Context } from '..'
-import { fetchOneFood } from '../http/foodAPI'
+import { addBasketFood, fetchOneFood } from '../http/foodAPI'
 
 const FoodPage = () => {
   const {user} = useContext(Context)
@@ -23,29 +23,29 @@ const FoodPage = () => {
   return (
     <Container className='d-flex justify-content-center align-items-start mt-5 pt-5' style={{height: window.innerHeight - 150}}>
       <>
-      <Form className='d-flex flex-column'>        
+      <div className='d-flex flex-column'>        
           <Image width={300} height={300} src={process.env.REACT_APP_API_URL + food.img} className="rounded"/>
           <Card
             className="mt-4 d-flex flex-column align-items-center justify-content-around"
             style={{width: 300, height: 100, fontSize: 32}}
           >
-            <h3 className='pt-2'>Цена: {food.price} руб</h3>
-            {user.role === "USER" && <Button variant={"outline-dark"} className="mb-3">Добавить в корзину</Button>}
+            <h3 className='pt-2'>Цена: {food.price} руб</h3>            
           </Card>  
-      </Form>
-      <Form className='d-flex flex-column align-items-center' style={{width: 700}}>
+          <Button variant={"outline-dark"} className="mt-4" onClick={() => addBasketFood(id)}>Добавить в корзину</Button>
+      </div>
+      <div className='d-flex flex-column align-items-center' style={{width: 700}}>
         <h1>{food.name}</h1>
         <h2 className="mt-1">Описание</h2>
         <Card className="mt-1 p-3" style={{width: 500}}>{food.description}</Card>
         <h2 className='mt-2'>Свойства</h2>
-        <Row className="d-flex flex-column" style={{width: 500}}>
+        <div className="d-flex flex-column" style={{width: 500}}>
           {food.info.map(info =>
-            <Form key={info.id}>
+            <div key={info.id}>
               {info.title}: {info.description}
-            </Form>
+            </div>
           )}
-        </Row>
-      </Form>
+        </div>
+      </div>
       </>  
     </Container>    
   )
